963265b53f5e4dfef50482828ca361a32e9e5a75
biwano <biwano@gmail.com>
2023-08-28 19:56:27 +0200

    Refactored the DetailPage Component

--- carbonmark-data/components/pages/DetailPage/index.tsx
+++ carbonmark-data/components/pages/DetailPage/index.tsx
@@ -2,15 +2,14 @@
  * A UI layout component to position detail pages content
  */
 export default function DetailPage(props: {
-  chart: React.ReactNode | Promise<React.ReactNode>;
+  card: React.ReactNode | Promise<React.ReactNode>;
   pageTitle: string;
-  chartTitle: string;
   overview: string;
 }) {
   return (
     <div>
       <h1>{props.pageTitle}</h1>
-      {props.chart}
+      {props.card}
       <div>{props.overview}</div>
     </div>
   );
