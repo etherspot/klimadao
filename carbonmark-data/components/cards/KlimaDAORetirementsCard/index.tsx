d82eb811a08ee19b0de235e8d38a16fc1cefd5bf
biwano <biwano@gmail.com>
2023-09-05 18:44:03 +0200

    Detailed list of KlimaDAO Retirements: all columns

--- carbonmark-data/components/cards/KlimaDAORetirementsCard/index.tsx
+++ carbonmark-data/components/cards/KlimaDAORetirementsCard/index.tsx
@@ -1,11 +1,16 @@
+import { formatTonnes } from "@klimadao/lib/utils/lightIndex";
 import { t } from "@lingui/macro";
-import Table, { Columns } from "components/Table";
+import PolyscanLink from "components/charts/helpers/PolygonscanLink";
+import Table, { Columns } from "components/charts/helpers/Table";
+import VerraProjectLink from "components/charts/helpers/VerraProjectLink";
 import { helpers } from "lib/charts";
 import { queryRawKlimaRetirements } from "lib/charts/queries";
 import { RawRetirementsItem } from "lib/charts/types";
 import { currentLocale } from "lib/i18n";
+import layout from "theme/layout.module.scss";
+import ChartCard, { CardProps } from "../ChartCard";
 /** Async server component that renders a Recharts client component */
-export default function KlimaDAORetirementsCard() {
+export default function KlimaDAORetirementsCard(props: CardProps) {
   const locale = currentLocale();
   function fetchFunction(page: number) {
     return queryRawKlimaRetirements({
@@ -17,11 +22,60 @@ export default function KlimaDAORetirementsCard() {
   }
   const columns: Columns<RawRetirementsItem> = [
     {
+      header: t`Beneficiary address`,
+      cellStyle: layout.textLeft,
+      dataKey: "beneficiary",
+      formatter: (x: string | number) => x,
+    },
+    {
+      header: t`Project`,
+      cellStyle: layout.textCenter,
+      dataKey: "project_id",
+      formatter: (projectId: string) => (
+        <VerraProjectLink projectId={projectId}></VerraProjectLink>
+      ),
+    },
+    {
+      header: t`Pool`,
+      cellStyle: layout.textCenter,
+      dataKey: "token",
+      formatter: (x: string | number) => x,
+    },
+    {
       header: t`Date`,
+      cellStyle: layout.textRight,
       dataKey: "retirement_date",
-      formatter: helpers.formatDateAndTime(locale),
+      formatter: helpers.formatDateAsDaysShort(locale),
+    },
+    {
+      header: t`Tonnes`,
+      cellStyle: layout.textRight,
+      dataKey: "quantity",
+      formatter: (amount: string | number) =>
+        formatTonnes({
+          amount: String(amount),
+          locale,
+          minimumFractionDigits: 2,
+        }),
+    },
+    {
+      header: "",
+      cellStyle: layout.textLeft,
+      dataKey: "transaction_id",
+      formatter: (transactionId: string) => (
+        <PolyscanLink transactionId={transactionId}></PolyscanLink>
+      ),
     },
   ];
   /* @ts-expect-error async Server component */
-  return <Table fetchFunction={fetchFunction} columns={columns}></Table>;
+  const chart = <Table fetchFunction={fetchFunction} columns={columns}></Table>;
+
+  return (
+    <ChartCard
+      {...props}
+      title={t`Detailed list of KlimaDAO Retirements`}
+      isColumnCard={true}
+      chart={chart}
+    />
+  );
 }
--- carbonmark-data/components/charts/helpers/Table/index.tsx
+++ carbonmark-data/components/charts/helpers/Table/index.tsx
@@ -1,9 +1,11 @@
 import { PaginatedResponse } from "lib/charts/types";
 import React from "react";
+import styles from "./styles.module.scss";
 
 export type Columns<RI> = Array<{
   header: string;
   dataKey: keyof RI;
+  cellStyle: string;
   formatter:
     | ((v: string) => string | React.ReactNode)
     | ((v: number) => string | React.ReactNode);
@@ -17,16 +19,18 @@ export default async function Table<RI>(props: {
   const page = 0;
   const data = await props.fetchFunction(page);
   return (
-    <table>
-      <tr>
+    <table className={styles.table}>
+      <tr className={styles.header}>
         {props.columns.map((column, index) => (
-          <th key={index}>{column.header}</th>
+          <th key={index} className={column.cellStyle}>
+            {column.header}
+          </th>
         ))}
       </tr>
       {data.items.map((item, index) => (
-        <tr key={index}>
+        <tr key={index} className={styles.row}>
           {props.columns.map((column, index) => (
-            <td key={index}>
+            <td key={index} className={column.cellStyle}>
               {column.formatter(item[column.dataKey] as never)}
             </td>
           ))}
